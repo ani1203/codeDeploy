73000c71e1a604d1dcc55fe1ec2e94bbca7583b7
ani1203 <46773492+ani1203@users.noreply.github.com>
2019-04-01 10:43:15 +0530

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,17 +2,17 @@
 sudo apt-get install python3-pip python3-dev nginx git -y
 pip3 install virtualenv -y
 virtualenv /home/ubuntu/venv
-chown ubuntu:ubuntu cds-demo
+chown ubuntu:ubuntu codeDeploy
 source /home/ubuntu/venv/bin/activate
-pip3 install -r /home/ubuntu/cds-demo/requirements/base.txt
+pip3 install -r /home/ubuntu/codeDeploy/requirements/base.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn -y
-python3 /home/ubuntu/cds-demo/chatApp/manage.py collectstatic
-cp /home/ubuntu/cds-demo/required-files/gunicorn.service /etc/systemd/system/
+python3 /home/ubuntu/codeDeploy/learnDjango/manage.py collectstatic
+cp /home/ubuntu/codeDeploy/required-files/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
-cp /home/ubuntu/cds-demo/required-files/chatApp /etc/nginx/sites-available/
+cp /home/ubuntu/codeDeploy/required-files/learnDjango /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/learnDjango /etc/nginx/sites-enabled
 sudo nginx -t
 sudo rm /etc/nginx/sites-enabled/default
